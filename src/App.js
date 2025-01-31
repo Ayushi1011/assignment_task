92e76c57b91aea599583c748ee49962972d5e374
ayushi <you@example.com>
2025-01-30 23:06:41 -0800

    sidebar creation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/DashBoard"; 
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
 
-function Layout() {
+function Layout(){
   return (
-    <div>Layout</div>
-  )
-}
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex flex-col flex-1">
+        <Navbar />
+        <main className="p-6 bg-gray-100 h-full overflow-auto">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
